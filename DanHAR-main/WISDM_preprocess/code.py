0c4573cd813495028f02c3e473d921d86047b46c
Eliauk-Aaron <76199987+Eliauk-Aaron@users.noreply.github.com>
2024-02-09 23:00:29 +0000

    Update code.py
    修改了preprocess的code.py,能够加载phone里面的acc数据集并分成test和train，储存在npy文件里

--- DanHAR-main/WISDM_preprocess/code.py
+++ DanHAR-main/WISDM_preprocess/code.py
@@ -3,6 +3,14 @@ import pandas as pd
 from scipy import stats
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+import argparse #pass the argument
+
+# 创建ArgumentParser对象
+parser = argparse.ArgumentParser(description='Process data path.')
+# 添加DATA_PATH参数
+parser.add_argument('DATA_PATH', type=str, help='The path to your data file')
+# args = parser.parse_args()  # 解析参数
+
 
 COLUMN_NAMES = [
     'user',
@@ -14,15 +22,41 @@ COLUMN_NAMES = [
 ]
 
 LABELS = [
-    'Downstairs',
-    'Jogging',
-    'Sitting',
-    'Standing',
-    'Upstairs',
-    'Walking'
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S'
 ]
 
-DATA_PATH = 'WISDM_ar_v1.1_raw.txt'
+# LABELS = [
+#     'Downstairs',
+#     'Jogging',
+#     'Sitting',
+#     'Standing',
+#     'Upstairs',
+#     'Walking'
+# ]
+
+# DATA_PATH = args.DATA_PATH
+# DATA_PATH = 'WISDM_ar_v1.1_raw.txt'
+
+base_path = "/content/drive/MyDrive/project_codes_data/wisdm-dataset/raw/phone/accel/"
+DATA_PATH = [f"{base_path}data_{i}_accel_phone.txt" for i in range(1600, 1651)]
+
 
 RANDOM_SEED = 13
 
@@ -39,19 +73,39 @@ SEGMENT_TIME_SIZE = 200
 
 if __name__ == '__main__':
 
-    # LOAD DATA
-    data = pd.read_csv(DATA_PATH, header=None, names=COLUMN_NAMES)
+    # # LOAD DATA
+    # data = pd.read_csv(DATA_PATH, header=None, names=COLUMN_NAMES)
+
+    # load data
+    data = pd.DataFrame()
+    # 遍历DATA_PATH列表中的每个文件路径
+    for file_path in DATA_PATH:
+      # 读取当前文件的数据
+      temp_data = pd.read_csv(file_path, header=None, names=COLUMN_NAMES)
+      # 将当前文件的数据追加到data DataFrame中
+      data = pd.concat([data, temp_data], ignore_index=True)
+
+
     data['z-axis'].replace({';': ''}, regex=True, inplace=True)
     data = data.dropna()
 
+    # # SHOW GRAPH FOR JOGGING
+    # data[data['activity'] == 'Jogging'][['x-axis']][:50].plot(subplots=True, figsize=(16, 12), title='Jogging')
+    # plt.xlabel('Timestep')
+    # plt.ylabel('X acceleration (dg)')
+
     # SHOW GRAPH FOR JOGGING
-    data[data['activity'] == 'Jogging'][['x-axis']][:50].plot(subplots=True, figsize=(16, 12), title='Jogging')
+    data[data['activity'] == 'H'][['x-axis']][:50].plot(subplots=True, figsize=(16, 12), title='Eating soap')
     plt.xlabel('Timestep')
     plt.ylabel('X acceleration (dg)')
 
+    # # SHOW ACTIVITY GRAPH
+    # activity_type = data['activity'].value_counts().plot(kind='bar', title='Activity type')
+    # # plt.show()
+
     # SHOW ACTIVITY GRAPH
     activity_type = data['activity'].value_counts().plot(kind='bar', title='Activity type')
-    # plt.show()
+    plt.show()
 
     # DATA PREPROCESSING
     data_convoluted = []
@@ -64,11 +118,15 @@ if __name__ == '__main__':
         z = data['z-axis'].values[i: i + SEGMENT_TIME_SIZE]
         data_convoluted.append([x, y, z])
 
+        # # Label for a data window is the label that appears most commonly
+        # label = stats.mode(data['activity'][i: i + SEGMENT_TIME_SIZE])[0][0]
+        # labels.append(label)
+
         # Label for a data window is the label that appears most commonly
-        label = stats.mode(data['activity'][i: i + SEGMENT_TIME_SIZE])[0][0]
+        label = data['activity'][i: i + SEGMENT_TIME_SIZE].value_counts().idxmax()
         labels.append(label)
 
-    # Convert to numpy
+    # Convert to numpy (x y z 的并列->堆叠)
     data_convoluted = np.asarray(data_convoluted, dtype=np.float32).transpose(0, 2, 1)
 
     # One-hot encoding
